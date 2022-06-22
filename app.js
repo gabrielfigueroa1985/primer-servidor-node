42f6a022cfbf7003091ae47e13eb01859bfc96ff
Gabriel Figueroa <gabrielfigueroa1946@gmail.com>
2022-06-22 10:59:26 -0300

    mi primer commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const { clear } = require("console");
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+app.listen(3000);
